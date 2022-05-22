b1456137726b9966e2f597717c1c34b9174b731d
Guilherme Tocci <gt-267@hotmail.com>
2022-05-22 18:08:12 -0300

    Edit1

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,14 +10,41 @@
  * 
  * 
  */
-   const pergunta= prompt("Quer iniciar uma nova rodada?").toLowerCase
-    console.log("Boas vindas ao jogo de Blackjack!")
-    funcaoDeConfirmar=()=>{
-   if (pergunta ==="sim"){
-      return true
 
-      }else{
-         return false
+let cartaZero
+let cartaUm
+let cartaDois
+let cartaTres
 
-      }
-    }
+
+if (confirm("Deseja iniciar uma partida?")===true){   
+       cartaZero = comprarCarta()
+       cartaUm = comprarCarta()
+       cartaDois = comprarCarta()
+       cartaTres = comprarCarta()    
+}else{
+    console.log(`O jogo acabou`)
+}
+const primeiraCarta = comprarCarta();
+const segundaCarta = comprarCarta();
+console.log(`Você possui as seguintes cartas :${primeiraCarta.texto} e ${segundaCarta.texto}.`)
+let somaCartaUser = primeiraCarta.valor + segundaCarta.valor
+console.log(`Sua pontuação ${somaCartaUser}.`)
+
+const primeiraCartaPc = comprarCarta();
+const segundaCartaPC = comprarCarta();
+console.log(`O computador possui as seguintes cartas : ${primeiraCartaPc.texto} e ${segundaCartaPC.texto}`)
+let somaCartaPc = primeiraCartaPc.valor + segundaCartaPC.valor
+console.log(`A pontuação do computador é :${somaCartaPc}`)
+      
+if(somaCartaPc>somaCartaUser && somaCartaPc<=21){
+   console.log(`O computador ganhou!`)
+
+}else if(somaCartaPc<somaCartaUser && somaCartaUser<=21){
+   console.log(`O usuário ganhou!`)
+
+}
+else{
+   console.log(`Empate`)
+
+}
