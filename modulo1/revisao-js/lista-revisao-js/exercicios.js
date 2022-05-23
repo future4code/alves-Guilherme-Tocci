1d47777f4f4d83a8c0bdd7b4fa5c179e0f584e6e
Guilherme Tocci <gt-267@hotmail.com>
2022-05-23 19:02:47 -0300

    revisao-js11

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -98,8 +98,16 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+    const atores = []
+  for (let i = 0; i <= filme.atores.length - 1; i++) {
+    if (filme.atores[i] != filme.atores[0]) {
+      filme.atores[i] = ' ' + filme.atores[i]
+    }
+  }
+  return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores}.`
 }
+   
+
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
