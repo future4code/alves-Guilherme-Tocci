824e02c8c45efc4d4ff04e1d94a959ab91ef1014
Guilherme Tocci <gt-267@hotmail.com>
2022-05-23 17:17:21 -0300

    revisaojs6

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -37,12 +37,14 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-    
+    return Math.max.apply(null,array)
+
   
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    
 
 }
 
