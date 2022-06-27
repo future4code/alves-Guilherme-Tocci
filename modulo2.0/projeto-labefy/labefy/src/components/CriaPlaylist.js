cb1c700789168d54e7dcac4fec9dd417aaefd8d7
Guilherme Tocci <gt-267@hotmail.com>
2022-06-27 02:16:37 -0300

    labefy

--- modulo2.0/projeto-labefy/labefy/src/components/CriaPlaylist.js
+++ modulo2.0/projeto-labefy/labefy/src/components/CriaPlaylist.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import styled from 'styled-components'
 
+const Container = styled.div`
+    text-align: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`
+const Botao = styled.button`
+    margin: 10px;
+    width: 150px;
+    height: 35px;
+    border-radius: 10px;
+    background-color: #2fab3d;
+    border-color: #4B7ED1;
+    color: white;
+    :hover{
+        top:-4px;
+        box-shadow:0 4px 3px #999;
+        background-color: white;
+        color: #4B7ED1;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
 
 export default class CriaPlaylist extends Component {
     state = {
@@ -32,15 +53,15 @@ export default class CriaPlaylist extends Component {
 
   render() {
     return (
-      <div>
-        <button>Suas playlists</button>
+      <Container>
+        <Botao>Suas playlists</Botao>
         <h1>Criar Playlist</h1>
         <h2>Digite o nome da sua Playlist</h2>
         <input placeholder='Nome'
         value={this.state.nome}
         onChange={this.addNome} ></input>
-      <button onClick={this.fazerCadastro}>Adicionar Playlist</button>
-      </div>
+      <Botao onClick={this.fazerCadastro}>Adicionar Playlist</Botao>
+      </Container>
     )
   }
 }
